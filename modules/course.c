af641dd9d6ec147b8ca92c6ff18998df3fb592ae
i_suckz <robinaddin@gmail.com>
2026-07-29 03:51:44 +0600

    Added individual semester GPA display

--- modules/course.c
+++ modules/course.c
@@ -2,20 +2,27 @@
 #include <string.h>
 #include "course.h"
 
-Course createCourse(char code[], char name[], double credit)
+Course createCourse(
+    const char code[],
+    const char name[],
+    double credit,
+    int semester)
 {
     Course course;
 
     strcpy(course.code, code);
     strcpy(course.name, name);
     course.credit = credit;
+    course.semester = semester;
 
     return course;
 }
 
 void viewCourse(Course course)
 {
-    printf("Code: %s\n", course.code);
-    printf("Name: %s\n", course.name);
-    printf("Credit: %.1f\n", course.credit);
+    printf("%s - %s | Credit: %.1f | Semester: %d\n",
+           course.code,
+           course.name,
+           course.credit,
+           course.semester);
 }
